a3b056d60116ec29a0390e390e9c88b178cd00c2
Gaurab KC <manishaadhikarikc@Manishas-MacBook-Pro.local>
2020-04-04 21:14:08 +0300

    exercise 1: Body Mass Index

---
+++ bmiCalculator.ts
@@ -0,0 +1,24 @@
+const bmiCalculator = (mass:number,height:number) => {
+    const bmi = mass/(height**2)
+    let text
+    switch (true) {
+        case (bmi < 18.5):
+            text = 'severly underweight'
+            break;
+        case (bmi < 25):
+            text = "normal"
+            break;
+        case (bmi < 30):
+            text = "overweight"
+            break;
+        case (bmi >= 35):
+            text = "obese"
+            break;
+        default:
+            text="please enter proper value"
+            break;
+    }
+    return text
+}
+
+console.log(bmiCalculator(81,1.92))
